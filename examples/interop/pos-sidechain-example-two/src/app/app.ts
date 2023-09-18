4f619fbd1abbd973a58ff53af77964d57c9cf52c
Ishan <ishantiw.quasar@gmail.com>
2023-09-18 18:12:54 +0200

    ♻️ Update sidechain two example

--- examples/interop/pos-sidechain-example-two/src/app/app.ts
+++ examples/interop/pos-sidechain-example-two/src/app/app.ts
@@ -4,15 +4,12 @@ import { registerPlugins } from './plugins';
 import { ReactModule } from './modules/react/module';
 
 export const getApplication = (config: PartialApplicationConfig): Application => {
-	const { app, method } = Application.defaultApplication(config, true);
+	const { app, method } = Application.defaultApplication(config, false);
 
 	const reactModule = new ReactModule();
 	app.registerModule(reactModule);
 	reactModule.addDependencies(method.interoperability);
-	const interoperabilityModule = app['_registeredModules'].find(
-		mod => mod.name === 'interoperability',
-	);
-	interoperabilityModule.registerInteroperableModule(reactModule);
+	app.registerInteroperableModule(reactModule);
 
 	registerModules(app);
 	registerPlugins(app);
